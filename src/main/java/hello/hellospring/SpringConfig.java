e17e1f1f3bd2226db4403e5405622c3df405a157
hwang-yeongil <jyhwangy4k@gmail.com>
2022-10-26 10:09:44 +0900

    plz set @Controller...

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,17 +1,10 @@
 package hello.hellospring;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 
 @Configuration
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
+@Controller
 public class MemberController {
 	private final MemberService memberService;
 	
@@ -38,5 +40,9 @@ public class MemberController {
 		List<Member> members = memberService.findMembers();
 		model.addAttribute("members", members);
 		return "members/memberList";
+	}
+	@GetMapping(value = "/mem")
+	public String aaa() {
+		return "mem";
 	}
 }
